db40fe8ae339fbb5657d93171d49e23416652824
phcatanduba <phcatanduba@gmail.com>
2021-04-02 14:08:22 -0300

    initial bonus layout

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const icon = document.querySelectorAll(".icon");
 const buttonCloseOrder = document.querySelector(".close-order");
 
 let applyStyleArray = [];
+let dish, dessert, drink;
 
 dishes.forEach(li => {
     listen(li, "dish");
@@ -24,9 +25,6 @@ function listen(li, liType) {
 }
 
 function changeLiStyle(li, liType) {
-    let dish    = applyStyleArray[0];
-    let dessert = applyStyleArray[1];
-    let drink   = applyStyleArray[2];
 
     switch(liType) {
         case 'dish': 
@@ -53,6 +51,9 @@ function changeLiStyle(li, liType) {
                 };
             break; 
     };
+    dish    = applyStyleArray[0];
+    dessert = applyStyleArray[1];
+    drink   = applyStyleArray[2];
    
 };
 
@@ -69,6 +70,8 @@ function addAndRemoveLiStyle(li, index) {
 
 function closeOrder() {
     let count = 0;
+    let wppMsg, encodeMsg;
+
     applyStyleArray.forEach(element => {
         if(element !== undefined) {
             count++;
@@ -77,7 +80,30 @@ function closeOrder() {
     if(count === 3) {
         buttonCloseOrder.classList.add("close-order-click");
         buttonCloseOrder.innerHTML = "Fechar pedido";
-    }
+
+        encodeMsg   = encodeURIComponent(message());
+        wppMsg      = "https://wa.me/351915507795?text=" + encodeMsg;
+        buttonCloseOrder.parentElement.setAttribute("href", wppMsg);
+    };
 };
 
+function message() {
+
+    dishPrice    = dish.children[0].querySelector(".price").getAttribute("value");
+    dessertPrice = dessert.children[0].querySelector(".price").getAttribute("value");
+    drinkPrice   = drink.children[0].querySelector(".price").getAttribute("value");
+
+    dishName     = dish.children[0].querySelector(".item-name").innerHTML
+    dessertName  = dessert.children[0].querySelector(".item-name").innerHTML
+    drinkName    = drink.children[0].querySelector(".item-name").innerHTML
+
+    let price = Number(dishPrice) + Number(dessertPrice) + Number(drinkPrice);
+    let msg = `Olá, gostaria de fazer o pedido:\n
+    - Prato: ${dishName}\n
+    - Bebida: ${drinkName}\n
+    - Sobremesa: ${dessertName}\n
+    Total: R$ ${price.toFixed(2)}`;
+
+    return msg;
+};
 
