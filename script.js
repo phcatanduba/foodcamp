2862f3ab5a262f12c8f6e11f02fa4cbaec590e89
phcatanduba <phcatanduba@gmail.com>
2021-04-01 22:14:12 -0300

    js, html and css refactor

--- script.js
+++ script.js
@@ -1,91 +1,83 @@
-const dishes = document.querySelectorAll(".dish-clicked");
-const desserts = document.querySelectorAll(".dessert-clicked");
-const drinks = document.querySelectorAll(".drink-clicked");
+const dishes = document.querySelectorAll(".dishes li");
+const desserts = document.querySelectorAll(".desserts li");
+const drinks = document.querySelectorAll(".drinks li");
 const icon = document.querySelectorAll(".icon");
 const buttonCloseOrder = document.querySelector(".close-order");
 
-let dishesCount = 0;
-let dessertsCount = 0;
-let drinksCount = 0;
-let previousLiDishes, previousLiDrinks, previousLiDesserts;
-
-function changeLiStyle(li, counter) {
-
-    switch(counter) {
-        case 'dishesCount':
-            if(dishesCount  === 0){
-                li.classList.add("li-clicked");
-                dishesCount++;
-                previousLiDishes = li;
-            }
-            else {
-                previousLiDishes.classList.remove("li-clicked");
-                li.classList.add("li-clicked");
-                previousLiDishes = li;
-            }
-            break;
-
-        case 'dessertsCount':
-
-            if(dessertsCount === 0){
-                li.classList.add("li-clicked");
-                dessertsCount++;
-                previousLiDesserts = li;
-            }
-            else {
-                previousLiDesserts.classList.remove("li-clicked");
-                li.classList.add("li-clicked");
-                previousLiDesserts = li;
-            }
-            break;
-            
-        case 'drinksCount':
-
-            if(drinksCount === 0){
-                li.classList.add("li-clicked");
-                drinksCount++;
-                previousLiDrinks = li;
-            }
-            else {
-                previousLiDrinks.classList.remove("li-clicked");
-                li.classList.add("li-clicked");
-                previousLiDrinks = li;
-            }
-            break;
-    };
-
-    const closeOrder = dishesCount === 1 && dessertsCount === 1 && drinksCount === 1;
-    if(closeOrder) {
-
-        buttonCloseOrder.innerHTML = "Fechar pedido";
-        buttonCloseOrder.classList.add("close-order-clicked");
-
-        buttonCloseOrder.addEventListener("click", function () {
-
-        });
-    };
-
-};
+let applyStyleArray = [];
 
 dishes.forEach(li => {
-    li.addEventListener("click", function() {
-        changeLiStyle(li, 'dishesCount');
-    });
+    listen(li, "dish");
 });
-
-
 desserts.forEach(li => {
-    li.addEventListener("click", function() {
-        changeLiStyle(li, 'dessertsCount');
-    });
+    listen(li, "dessert");
 });
-
-
 drinks.forEach(li => {
-    li.addEventListener("click", function() {
-        changeLiStyle(li, 'drinksCount');
-    });
+    listen(li, "drink");
 });
 
+function listen(li, liType) {
+    li.addEventListener("click", function() {
+        changeLiStyle(li, liType);
+        closeOrder();
+    });
+}
+
+function changeLiStyle(li, liType) {
+    let dish    = applyStyleArray[0];
+    let dessert = applyStyleArray[1];
+    let drink   = applyStyleArray[2];
+
+    switch(liType) {
+        case 'dish': 
+            if(dish === undefined) {
+                addLiStyle(li, 0)
+            }   else if(dish.className === "li-clicked") {
+                    addAndRemoveLiStyle(li, 0);
+                };
+            break; 
+
+        case 'dessert': 
+            if(dessert === undefined) {
+                addLiStyle(li, 1);
+            }   else if(dessert.className === "li-clicked") {
+                    addAndRemoveLiStyle(li, 1);
+                };
+            break;
+
+        case 'drink': 
+            if(drink === undefined) {
+                addLiStyle(li, 2);
+            }   else if(drink.className === "li-clicked") {
+                    addAndRemoveLiStyle(li, 2);
+                };
+            break; 
+    };
+   
+};
+
+function addLiStyle(li, index) {
+    li.classList.add("li-clicked");
+    applyStyleArray[index] = li;
+};
+
+function addAndRemoveLiStyle(li, index) {
+    applyStyleArray[index].classList.remove("li-clicked");
+    li.classList.add("li-clicked");
+    applyStyleArray[index] = li;
+}
+
+function closeOrder() {
+    let count = 0;
+    applyStyleArray.forEach(element => {
+        if(element !== undefined) {
+            count++;
+        }
+    });
+    if(count === 3) {
+        buttonCloseOrder.classList.add("close-order-click");
+        buttonCloseOrder.innerHTML = "Fechar pedido";
+    }
+};
 
 
